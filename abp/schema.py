9567fce4cdb489dbd9486c8b7bc0b969bfef5683
Bruno L. Carli <brunolcarli@gmail.com>
2019-06-24 23:10:55 -0300

    ADD Trainer class and creaation

--- abp/schema.py
+++ abp/schema.py
@@ -1,9 +1,76 @@
 import graphene
+from abp.models import Quote, Trainer, Badges, Elite
+
+class TrainerType(graphene.ObjectType):
+    '''
+    Objeto GraphQl para um Trainer.
+    '''
+    id = graphene.ID()
+    name = graphene.String()
+    nickname = graphene.String()
+    is_winner = graphene.Boolean()
+    num_wins = graphene.Int()
+    num_losses = graphene.Int()
+    num_battles = graphene.Int()
+    badges = graphene.List(graphene.String)
+
 
 class Query(object):
     '''
     Queries para a aplicação ABP.
     '''
-    teste = graphene.String()
-    def resolve_teste(self, info, **kwargs):
-        return 'ABP working'
+    abp_quotes = graphene.List(graphene.String)
+    def resolve_abp_quotes(self, info, **kwargs):
+        quotes = Quote.objects.all()
+        return [quote.quote for quote in quotes]
+
+
+class CreateAbpQuote(graphene.relay.ClientIDMutation):
+    '''
+    Registra um quote no banco de dados.
+    '''
+    response = graphene.String(
+        description='Create a text quote on the database.'
+    )
+
+    class Input:
+        quote = graphene.String(
+            required=True,
+            description='A quote to be forever remembered.'
+        )
+
+    def mutate_and_get_payload(self, info, **_input):
+
+        quote = _input.get('quote')
+        registry = Quote.objects.create(quote=quote)
+        registry.save()
+        return CreateAbpQuote(registry.quote)
+
+
+class CreateTrainer(graphene.relay.ClientIDMutation):
+    '''
+    Registra um treinador no banco de dados.
+    '''
+
+    trainer = graphene.Field(TrainerType)
+
+    class Input:
+        name = graphene.String()
+        nickname = graphene.String()
+
+    def mutate_and_get_payload(self, info, **_input):
+        name = _input.get('name')
+        nickname = _input.get('nickname')
+        trainer = Trainer.objects.create(
+            name=name,
+            nickname=nickname,
+        )
+        
+
+        trainer.save()
+        return CreateTrainer(trainer)
+
+
+class Mutation:
+    create_abp_quote = CreateAbpQuote.Field()
+    create_trainer = CreateTrainer.Field()

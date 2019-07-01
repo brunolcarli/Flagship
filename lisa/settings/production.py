121111e4fd611efad91a4d9d7146f9ab76ae07ef
Bruno L. Carli <brunolcarli@gmail.com>
2019-06-30 21:57:04 -0300

    CHANGE prod sets

--- lisa/settings/production.py
+++ lisa/settings/production.py
@@ -1,7 +1,7 @@
 import os
 from decouple import config
 from lisa.settings.common import *
-
+import dj_database_url
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
@@ -16,22 +16,22 @@ from lisa.settings.common import *
 
 #configs para o heroku
 cwd = os.getcwd()
-if cwd == '/app' or cwd[:4] == '/tmp':
-    import dj_database_url
-    DATABASES = {
-    'default':dj_database_url.config(default='postgres://localhost')
-    }
-    # Honra o cabecalho 'X-forwarded-proto' para request.is_secure()
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-    #cabecalhos para permitir todos os hosts
-    ALLOWED_HOSTS = ['*']
-    DEBUG = True
-    #configs de recursos estaticos
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'staticfiles'
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
-    )
+# if cwd == '/app' or cwd[:4] == '/tmp':
+    
+DATABASES = {
+'default':dj_database_url.config(default='postgres://localhost')
+}
+# Honra o cabecalho 'X-forwarded-proto' para request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+#cabecalhos para permitir todos os hosts
+ALLOWED_HOSTS = ['*']
+DEBUG = True
+#configs de recursos estaticos
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
